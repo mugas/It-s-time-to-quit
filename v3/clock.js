09fa3ecac1b3ac990637c5708fb9d6250f973d53
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2018-09-12 00:06:26 +0300

    add new version where put the functionality working of the digital clock

---
+++ v3/clock.js
@@ -0,0 +1,68 @@
+
+var week = document.querySelector("#digitalDate");
+var secondHand = document.querySelector(".second-hand");
+var minuteHand = document.querySelector(".min-hand");
+var hourHand = document.querySelector(".hour-hand");
+var hourDigital = document.querySelector(".hour");
+var minuteDigital = document.querySelector(".minute");
+var secondDigital = document.querySelector(".second");
+var milisecondDigital = document.querySelector(".milisecond");
+
+
+//Setting the week
+
+function setWeek(){
+	var today = moment();
+	var todayDate = today.format("dddd, "+ "D " + "MMMM " + "Of " + "YYYY");
+	week.textContent = todayDate;
+}
+setWeek();
+
+
+function analogClock(){
+	var now= new Date();
+	var seconds = now.getSeconds();
+	var secondsDegree = ((seconds / 60) * 360) + 90;
+	secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+	
+
+	var minutes = now.getMinutes();
+	var minutesDegree = ((minutes / 60 * 360) + 90);
+	minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
+
+	var hour = now.getHours();
+	var hourDegree = ((hour / 12 * 360) + 90);
+	hourHand.style.transform = `rotate(${hourDegree}deg)`;
+}
+
+function digitalClock(){
+	var timezone = moment.tz.guess();
+	var now = moment();
+	var hour = now.format("HH");
+	var minute = now.format("mm");
+	var second = now.format("ss");
+	var milisecond = moment().millisecond();
+	hourDigital.textContent = hour;
+	minuteDigital.textContent = minute;
+	secondDigital.textContent = second;
+	milisecondDigital.textContent = milisecond;
+}
+
+
+
+
+
+setInterval(analogClock, 1000);
+setInterval(setWeek, 1000);
+setInterval(digitalClock, 1000);
+analogClock();
+digitalClock();
+
+
+
+
+
+ 
+
+ 
+//https://www.youtube.com/watch?v=KRnkXgbbpzY
