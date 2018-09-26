4a8d7c63d68f0d933265668be20f45336fc23d0c
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2018-09-26 23:25:25 +0300

    ending the project

--- readme.md
+++ readme.md
@@ -1,6 +1,23 @@
 ## Clock and Time Zones
 
-PLaying with time and time zones.
-I'm using the library moments.
-Also working with DOM Manipulation
+A clock made with JS and CSS.
+For the digital clock I worked with momentjs.
 
+With JS I worked and learn with:
+
+<ul>
+<li>Functions</li>
+<li>Scopes</li>
+<li>Methods</li>
+<li>Libraries</li>
+<li>Time variables</li>
+</ul>
+
+In CSS i worked and learn with:
+
+<ul>
+<li>Animations</li>
+<li>Variables</li>
+<li>Positions</li>
+<li>Libraries</li>
+</ul>
--- v4/clock.js
+++ v4/clock.js
@@ -1,6 +1,5 @@
-
 var week = document.querySelector("#digital-date");
-var timings = document.querySelector("div span");
+var timings = document.querySelector("#city");
 var secondHand = document.querySelector(".second-hand");
 var minuteHand = document.querySelector(".min-hand");
 var hourHand = document.querySelector(".hour-hand");
@@ -8,71 +7,51 @@ var hourDigital = document.querySelector(".hour");
 var minuteDigital = document.querySelector(".minute");
 var secondDigital = document.querySelector(".second");
 
-
-
-
-
 //Setting the week
 
-function setWeek(){
-	var today = moment();
-	var todayDate = today.format("dddd, "+ "D " + "MMMM " + "Of " + "YYYY");
-	week.textContent = todayDate;
+function setWeek() {
+  var today = moment();
+  var todayDate = today.format("dddd, " + "D " + "MMMM " + "Of " + "YYYY");
+  week.textContent = todayDate;
 }
 setWeek();
 
-function timeZone(){
-var teste = moment.tz.guess();
-console.log(teste);
-timings.textContent = teste;
+function timeZone() {
+  var teste = moment.tz.guess();
+  console.log(teste);
+  timings.textContent = teste;
 }
 
 timeZone();
 
+function analogClock() {
+  var now = new Date();
+  var seconds = now.getSeconds();
+  var secondsDegree = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegree}deg)`;
 
-function analogClock(){
-	var now= new Date();
-	var seconds = now.getSeconds();
-	var secondsDegree = ((seconds / 60) * 360) + 90;
-	secondHand.style.transform = `rotate(${secondsDegree}deg)`;
-	
+  var minutes = now.getMinutes();
+  var minutesDegree = (minutes / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
 
-	var minutes = now.getMinutes();
-	var minutesDegree = ((minutes / 60 * 360) + 90);
-	minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
-
-	var hour = now.getHours();
-	var hourDegree = ((hour / 12 * 360) + 90);
-	hourHand.style.transform = `rotate(${hourDegree}deg)`;
+  var hour = now.getHours();
+  var hourDegree = (hour / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourDegree}deg)`;
 }
 
-function digitalClock(){
-	var timezone = moment.tz.guess();
-	var now = moment();
-	var hour = now.format("HH");
-	var minute = now.format("mm");
-	var second = now.format("ss");
-	hourDigital.textContent = hour;
-	minuteDigital.textContent = minute;
-	secondDigital.textContent = second;
+function digitalClock() {
+  var timezone = moment.tz.guess();
+  var now = moment();
+  var hour = now.format("HH");
+  var minute = now.format("mm");
+  var second = now.format("ss");
+  hourDigital.textContent = hour;
+  minuteDigital.textContent = minute;
+  secondDigital.textContent = second;
 }
-
- 
-
-
-
 
 setInterval(analogClock, 1000);
 setInterval(setWeek, 1000);
 setInterval(digitalClock, 1000);
 analogClock();
 digitalClock();
-
-
-
-
-
- 
-
- 
-//https://www.youtube.com/watch?v=KRnkXgbbpzY
